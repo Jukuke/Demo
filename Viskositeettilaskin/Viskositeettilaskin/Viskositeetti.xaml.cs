018b1cd543487d36efd2db7308af85690cbc655e
Juho Kemppainen <juhkuskem@hotmail.com>
2019-01-29 22:51:29 +0200

    Ensimmäinen vaihe

---
+++ Viskositeettilaskin/Viskositeettilaskin/Viskositeetti.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Viskositeettilaskin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlCeConnection con = new SqlCeConnection(Properties.Settings.Default.NorthwindConnectionString);
+        SqlCeCommand cmd;
+
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindows_load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+
+    internal class SqlCeCommand
+    {
+    }
+
+internal class SqlCeConnection
+{
+    public SqlCeConnection(string northwindConnectionString)
+    {
+    }
+}
+}
